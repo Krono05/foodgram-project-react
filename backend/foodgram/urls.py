12477be14b0eb7b220cd65a4e382f534ced79384
Тамир Болданов <rpp-2011@yandex.ru>
2021-07-28 18:22:01 +0300

    deploy7

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('recipes.urls')),
     path('api/', include('users.urls')),
 ]
