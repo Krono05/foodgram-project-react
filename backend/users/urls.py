cc9e530920bc7299781198fdb78256e079d0ac56
Тамир Болданов <rpp-2011@yandex.ru>
2021-07-28 18:31:11 +0300

    deploy8

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import include
-from django.contrib import admin
+from django.urls import path
 
 from .views import FollowViewSet, ListFollowViewSet, logout, obtain_auth_token
 
-#from django.urls import path
-
-
 urlpatterns = [
-    #path('admin/', admin.site.urls),
     path(
         'users/subscriptions/',
         ListFollowViewSet.as_view(),
