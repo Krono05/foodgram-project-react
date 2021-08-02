8d4d2bd3c14bc6d2d8550ad1b938cef7bd61e2fe
Тамир Болданов <rpp-2011@yandex.ru>
2021-08-02 11:14:45 +0300

    change limit

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -81,7 +81,7 @@ class Recipe(models.Model):
     text = models.TextField(verbose_name='Описание')
     cooking_time = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1, message= "время не может быть отрицательным"), ],
-        verbose_name='Время приготовления'
+        verbose_name='Время приготовлений'
     )
     ingredients = models.ManyToManyField(
         Ingredient,
