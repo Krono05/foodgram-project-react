70ce80747922c45d2fb04c9f0839de8d0f6bc460
Тамир Болданов <rpp-2011@yandex.ru>
2021-08-02 18:42:34 +0300

    isort

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
