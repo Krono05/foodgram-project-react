fa38c2efc890c09367650ad1b08cb82b7f203a00
Тамир Болданов <rpp-2011@yandex.ru>
2021-08-04 13:20:02 +0300

    change filters 1

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -23,7 +23,7 @@ class RecipeFilter(filters.FilterSet):
     def get_favorite(self, queryset, name, value):
         user = self.request.user
         if value:
-            return Recipe.objects.filter(favorites__user=user)
+            return Recipe.objects.filter(favorites__user=user).distinct()
         return Recipe.objects.all()
 
     def get_is_in_shopping_cart(self, queryset, name, value):
